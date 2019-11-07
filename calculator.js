dda4f1ae46f44f5a2e612c6979bae30525e0a022
Charles Bonoan <charlesbonoan@gmail.com>
2019-11-06 22:53:02 -0800

    modified:   calculator.css 	modified:   calculator.html 	modified:   calculator.js

--- calculator.js
+++ calculator.js
@@ -14,3 +14,32 @@ function cancel(){
     str = str.slice(0,str.length-1)
     document.getElementById("output").value = str;
 }
+
+function neg(){
+    var expression = document.getElementById("output").value;
+    if(!expression.includes("-")){
+        var newExp = "-"+document.getElementById("output").value
+        document.getElementById("output").value = newExp
+    }
+    else{
+        var newExp = expression.slice(1);
+        document.getElementById("output").value = newExp;
+    }
+    
+}
+
+function calc(){
+    var err = "Invalid Expression"
+    if(document.getElementById("output").value != ""){
+        try{
+            var expression = document.getElementById("output").value;
+            document.getElementById("output").value = eval(expression);
+        }
+        catch(SyntaxError){
+            document.getElementById("output").value = err;
+        }
+    }
+    else{
+        document.getElementById("output").value = ""
+    }
+}
